272192b2c7d5bb5f0267a5972df9d8bff1c48896
bathaei <javadbat@gmail.com>
2018-02-09 22:32:49 +0330

    fs

---
+++ Skyhue/gulpfile.js
@@ -0,0 +1,29 @@
+﻿
+
+var gulp = require('gulp');
+var cssmin = require("gulp-cssmin");
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+var precss = require('precss');
+//var preCss = require('precss');
+
+gulp.task('default', defaultTask);
+gulp.task('cssMinifier', cssMinifier);
+
+function cssMinifier(done) {
+    console.log('start pipiline for css minifier');
+    var postCssTools = [precss]
+    return gulp.src(["wwwroot/css/*.css","*.css"])
+        .pipe(cssmin())
+        .pipe(postcss(postCssTools))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest("./minifycss/"));
+    //return tasks;
+   // done();
+}
+
+
+function defaultTask(done) {
+    // place code for your default task here
+    done();
+}
